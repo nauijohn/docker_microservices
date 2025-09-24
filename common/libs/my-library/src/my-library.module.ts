68ef8efc3a8d68375642b6af8c0b9a1fcb293ec1
nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-09-25 02:18:59 +1000

    updated version

--- common/libs/my-library/src/my-library.module.ts
+++ common/libs/my-library/src/my-library.module.ts
@@ -1,10 +1,17 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
 
 import { JwtAuthGuard } from "./jwt-auth.guard";
 import { JwtStrategy } from "./jwt.strategy";
 import { MyLibraryService } from "./my-library.service";
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: "topSecretKey",
+      signOptions: { expiresIn: "60s" },
+    }),
+  ],
   providers: [MyLibraryService, JwtAuthGuard, JwtStrategy],
   exports: [MyLibraryService, JwtAuthGuard, JwtStrategy],
 })
