9dcff122aab4f04ee8b3d4a58ec4665f86485092
nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-09-25 00:08:38 +1000

    common

--- common/libs/my-library/src/index.ts
+++ common/libs/my-library/src/index.ts
@@ -1,2 +1,5 @@
 export * from "./my-library.module";
 export * from "./my-library.service";
+
+export * from "./jwt-auth.guard";
+export * from "./jwt.strategy";
--- common/libs/my-library/src/my-library.module.ts
+++ common/libs/my-library/src/my-library.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 
+import { JwtAuthGuard } from "./jwt-auth.guard";
+import { JwtStrategy } from "./jwt.strategy";
 import { MyLibraryService } from "./my-library.service";
 
 @Module({
-  providers: [MyLibraryService],
-  exports: [MyLibraryService],
+  providers: [MyLibraryService, JwtAuthGuard, JwtStrategy],
+  exports: [MyLibraryService, JwtAuthGuard, JwtStrategy],
 })
 export class MyLibraryModule {}
