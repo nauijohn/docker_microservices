a4077699042253da5ea4c587f127c9cd20bbe21f
nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-09-25 23:42:33 +1000

    update version

--- common/libs/filters/token-expired-exception.filter.ts
+++ common/libs/filters/token-expired-exception.filter.ts
@@ -16,6 +16,8 @@ export class TokenExpiredExceptionFilter implements ExceptionFilter {
     const { httpAdapter } = this.httpAdapterHost;
     const ctx = host.switchToHttp();
 
+    console.log("exception: ", exception);
+
     const httpStatus =
       exception instanceof HttpException
         ? exception.getStatus()
