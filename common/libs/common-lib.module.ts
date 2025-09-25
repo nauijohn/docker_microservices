98bd582a19649081a8983151e5ee579db615bd17
nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-09-25 22:48:14 +1000

    update version

--- common/libs/common-lib.module.ts
+++ common/libs/common-lib.module.ts
@@ -1,9 +1,15 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
 import { JwtAuthGuard } from "./jwt-auth.guard";
 import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   providers: [JwtAuthGuard, JwtStrategy],
   exports: [JwtAuthGuard, JwtStrategy],
 })
