4fce11f1ba6961f7d6766716f0f8b38a3ec20e67
nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-09-25 15:09:24 +1000

    updated version

--- common/libs/common-lib.module.ts
+++ common/libs/common-lib.module.ts
@@ -1,17 +1,10 @@
 import { Module } from "@nestjs/common";
-import { JwtModule } from "@nestjs/jwt";
 
 import { JwtAuthGuard } from "./jwt-auth.guard";
 import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
-  imports: [
-    JwtModule.register({
-      secret: "superSecretKey",
-      signOptions: { expiresIn: "60m" },
-    }),
-  ],
   providers: [JwtAuthGuard, JwtStrategy],
-  exports: [JwtAuthGuard, JwtStrategy, JwtModule],
+  exports: [JwtAuthGuard, JwtStrategy],
 })
 export class CommonLibModule {}
